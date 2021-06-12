5896933f9455c59da79bc0ec9ccf3467331e12e5
abheeth99 <Abheethk@embla.asia>
2021-06-12 10:11:50 +0530

    reset all components

--- inventory-management-web/src/App.tsx
+++ inventory-management-web/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import InventoriesContextProvider from './Store/inventory-context';
-import InventoryList from './Components/InventoryList';
-
 function App() {
   return (
-    <InventoriesContextProvider>
-      <InventoryList/>
-    </InventoriesContextProvider>
+    <div>
+      APP
+    </div>
   );
 }
 
