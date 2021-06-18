46ac935bf11c340db673f9ba3e75696f87d9c464
Kanishka Hewapathirana <KanishkaH@embla.asia>
2021-06-18 10:24:58 +0530

    Fix merge conflicts

--- inventory-management-web-admin/src/api/TokenApi.ts
+++ inventory-management-web-admin/src/api/TokenApi.ts
@@ -1,4 +1,4 @@
-import Utility from '../Models/Utility';
+import Utility from '../Model/Utility';
 
 export const setToken = (token: Utility):Promise<any> =>{
     return fetch("https://localhost:44368/token",{
